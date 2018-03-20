d194468a1d6d5063b7f09d467586320a08e2145b
Brian Nyamu <brian.nyamu@flightdataservices.com>
2018-03-20 09:34:23 +0000

    AUTO: Added how to install polybar

--- auto_setup.sh
+++ auto_setup.sh
@@ -16,3 +16,12 @@ sudo make install
 #	oh-my-zsh
 #	vim + plugins
 #	fonts
+# Install polybar and its dependencies 
+sudo apt install -y xcb-proto xcb-util-image xcb-util-wm xcb-util-xrm libxcb-ewmh-dev python-xcbgen
+
+wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu yakkety-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
+sudo apt-get update
+sudo apt-get install polybar
+
+
