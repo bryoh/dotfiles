a3e9264b5283081813ae4813e850c62ed79376a1
bryoh <brianbryo@gmail.com>
2018-03-25 00:04:06 +0000

    POLYBAR: Match colours

--- i3/.config/polybar/whats_playing.sh
+++ i3/.config/polybar/whats_playing.sh
@@ -11,9 +11,9 @@ fi
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F#D08770}$metadata"       # Orange when playing
+    echo "%{F#ffa419}$metadata"       # Orange when playing
 elif [[ $player_status = "Paused" ]]; then
-    echo "%{F#65737E}$metadata"       # Greyed out info when paused
+    echo "%{F#42321b}$metadata"       # Greyed out info when paused
 else
-    echo "%{F#65737E}$icon"                 # Greyed out icon when stopped
+    echo "%{F#A86C11}$icon"                 # Greyed out icon when stopped
 fi
