de03447c7a781931f57ee9bd53c3e2e022368584
bryoh <brianbryo@gmail.com>
2018-03-28 16:12:42 +0100

    AUTOSETUP: Zsh stuff

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
 #	Automated script to install and setup
+#	zsh
+sudo apt install -y zsh
+sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
+sudo apt install -y git wget stow curl 
+sudo apt install -y neovim vim 
+sudo apt install -y taskwarrior fonts-powerline powerline 
+git clone https://github.com/bryoh/dotfiles.git ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -L git.io/antigen > antigen.zsh
+cd ~/dotfiles
+rm -rf ~/.zshrc # zsh will create an .zshrc file by default but we do not want it we want our own
+stow zsh vim 
+cd ~
+vim +PluginInstall +qall # install vim plugins
 #	i3
+sudo apt install -y i3 
+sudo at install -y xclip xsel 
 # 	i3gaps
 sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
 git clone https://www.github.com/Airblader/i3 ${HOME}/i3-gaps 
@@ -12,7 +29,6 @@ mkdir -p build && cd build
 ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
 make
 sudo make install 
-#	zsh
 #	oh-my-zsh
 #	vim + plugins
 #	fonts
@@ -22,6 +38,7 @@ sudo apt install -y xcb-proto xcb-util-image xcb-util-wm xcb-util-xrm libxcb-ewm
 wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu yakkety-getdeb apps" >> /etc/apt/sources.list.d/getdeb.list'
 sudo apt-get update
-sudo apt-get install polybar
-
-
+sudo apt-get install -y polybar
+# install oh my zsh
+cd ~
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
