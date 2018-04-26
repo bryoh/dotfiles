3b02e3d4a384cec84b491cecab48fb389620b43f
bryoh <brianbryo@gmail.com>
2018-04-26 15:30:35 +0100

    Polybar: Spotify green

--- i3/.config/polybar/whats_playing.sh
+++ i3/.config/polybar/whats_playing.sh
@@ -3,6 +3,11 @@
 # Specifying the icon(s) in the script
 # This allows us to change its appearance conditionally
 icon=""
+player_color="%F{ffa419}"
+
+if [[ $(playerctl metadata) == *'spotify'* ]]; then
+	player_color='%{F#1db954}'
+fi
 
 player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
@@ -11,7 +16,7 @@ fi
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F#ffa419}$metadata"       # Orange when playing
+    echo "%{F#$player_color$metadata"       # Orange when playing
 elif [[ $player_status = "Paused" ]]; then
     echo "%{F#42321b}$metadata"       # Greyed out info when paused
 else
