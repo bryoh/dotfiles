d253185e0250c62f8f310330ec53978d9b38b675
brian.nyamu <brianbryo@gmail.com>
2024-08-28 17:22:22 +0100

    neovim tool for commenting

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-
+    { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "plugins" },
   },
   defaults = {
