68705f20b032aef1456482ba4cd2927ffd21cbe8
bryoh <brianbryo@gmail.com>
2024-10-20 20:47:07 +0100

    nvim: lualine to display the path and filename

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -55,6 +55,9 @@ return {
         theme = my_lualine_theme,
       },
       sections = {
+        lualine_c = {
+          { "filename", path = 1 },
+        },
         lualine_x = {
           {
             lazy_status.updates,
