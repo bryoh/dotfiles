75b5ad23c4e541d8c07b66716802eb185514e360
brian.nyamu <brianbryo@gmail.com>
2025-03-10 09:43:49 +0000

    nvim:pydoc

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,4 +2,19 @@ return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   "xiyaowong/transparent.nvim",
+  {
+    "fredrikaverpil/pydoc.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim" }, -- optional
+      { "folke/snacks.nvim" }, -- optional
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+          ensure_installed = { "markdown" },
+        },
+      },
+    },
+    cmd = { "PyDoc" },
+    opts = {},
+  },
 }
