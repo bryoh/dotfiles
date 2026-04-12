386b1a7db6c526118199d02c1a0d6cedd5496c20
brian.nyamu <brianbryo@gmail.com>
2026-04-12 02:45:29 +0100

    feat: advanced mini.files config with project bookmarks

--- nvim/.config/nvim/lua/plugins/mini-files.lua
+++ nvim/.config/nvim/lua/plugins/mini-files.lua
@@ -1,15 +1,21 @@
 return {
-  "nvim-mini/mini.files",
+  "echasnovski/mini.files",
   opts = {
     mappings = {
-      go_in_plus = "",
+      go_in_plus = "L",
+      go_out_plus = "H",
       scroll_preview_down = "<C-d>",
       scroll_preview_up = "<C-u>",
     },
-
     windows = {
       preview = true,
       width_preview = 90,
     },
+  },
+  keys = {
+    { "<leader>fm", function() require("mini.files").open(vim.api.nvim_buf_get_name(0), true) end, desc = "Open mini.files (Directory of Current File)" },
+    { "<leader>fM", function() require("mini.files").open(vim.uv.cwd(), true) end, desc = "Open mini.files (CWD)" },
+    { "<leader>fd", function() require("mini.files").open(vim.fn.expand("~/dotfiles"), true) end, desc = "Open mini.files at ~/dotfiles" },
+    { "<leader>ft", function() require("mini.files").open(vim.fn.expand("~/ax-livia/test"), true) end, desc = "Open mini.files at ~/ax-livia/test" },
   },
 }
