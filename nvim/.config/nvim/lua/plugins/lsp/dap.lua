7bf87d85583c519890f84633dd170014dfa18797
brian.nyamu <brianbryo@gmail.com>
2026-04-13 12:24:56 +0100

    feat: refine cpp debugging with codelldb

--- nvim/.config/nvim/lua/plugins/lsp/dap.lua
+++ nvim/.config/nvim/lua/plugins/lsp/dap.lua
@@ -352,6 +352,16 @@ return {
           args = {},
         },
       }
+      table.insert(dap.configurations.cpp, {
+        name = "Launch file (codelldb)",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+      })
     end,
   },
   {
