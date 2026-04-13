fb1b7bb6fc4f6fe933d9895a76f42f14ba6f20eb
brian.nyamu <brianbryo@gmail.com>
2026-04-13 15:25:29 +0100

    fix: switch to native nvim osc52 clipboard provider

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -14,26 +14,16 @@ vim.g.maplocalleader = [[\]]
 -- OSC 52 clipboard support (SSH + Tmux compatible)
 if vim.env.SSH_TTY or vim.env.SSH_CONNECTION or vim.env.TMUX then
   vim.opt.clipboard = "unnamedplus"
-
-  local function copy(lines, _)
-    local s = table.concat(lines, "\n")
-    local b64 = vim.fn.system("base64 | tr -d '\n'", s)
-    local osc = "\x1b]52;c;" .. b64 .. "\x07"
-    if vim.env.TMUX then
-      osc = "\x1bPtmux;\x1b" .. osc .. "\x1b\\"
-    end
-    io.stdout:write(osc)
-  end
-
-  local function paste()
-    -- Note: Standard OSC 52 doesn't support pasting FROM local to remote easily
-    -- We fallback to reading the unnamed register
-    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-  end
-
   vim.g.clipboard = {
     name = "OSC 52",
-    copy = { ["+"] = copy, ["*"] = copy },
-    paste = { ["+"] = paste, ["*"] = paste },
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
   }
 end
+
